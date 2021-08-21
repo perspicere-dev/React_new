a572181cfdc32ff1caba56fe6b85dcc5ce3ce9d0
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-08-21 16:57:43 +0200

    props img

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero'
+import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+      title: PropTypes.node.isRequired,
+      children: PropTypes.node,
+      image: PropTypes.string,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+  
   render() {
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title}/>
+        <Hero titleText={this.props.title}
+              imageHero={this.props.image} />
         <div className={styles.description}>
           {this.props.children}
         </div>
