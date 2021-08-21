d91ea40fea62c24e5dd102b45083b6777a7fb666
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-08-21 16:08:10 +0200

    first task - props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import List from '../List/List';
+import styles from './App.scss'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My first React app</h1>
-        <h2>Hello world!</h2>
-      </div>
+      <main className={styles.component}>
+        <h1 className={styles.title}>My first React app</h1>
+        <h2 className={styles.subtitle}>Hello world!</h2>
+        <List title='Things to do' />
+      </main>
     )
   }
 }
