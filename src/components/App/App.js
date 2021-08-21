d315c7cd8e387d1efc40f3f8b4455689fb679c67
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-08-21 12:29:21 +0200

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My first React app</h1>
+        <h2>Hello world!</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
