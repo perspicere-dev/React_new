b7cc48bed3c5cdb9061d9755fe7fc3ff7fc7d6d7
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-09-11 13:39:15 +0200

    work on mapStateToProps, delete state and addColumn in List

--- src/components/App/AppContainer.js
+++ src/components/App/AppContainer.js
@@ -3,6 +3,9 @@ import App from './App';
 
 const mapStateToProps = state => ({
   title: state.app.title,
+  subtitle: state.app.subtitle,
+  lists: state.lists,
 });
+
 
 export default connect(mapStateToProps)(App);
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,15 +2,12 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column';
+// import Column from '../Column/Column';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator';
+// import Creator from '../Creator/Creator';
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  }
 
   static propTypes = {
     title: PropTypes.node.isRequired,
@@ -22,22 +19,6 @@ class List extends React.Component {
   static defaultProps = {
     description: settings.defaultListDescription,
   }
-  
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
 
   render() {
     return (
@@ -47,14 +28,14 @@ class List extends React.Component {
         <div className={styles.description}>
           {ReactHtmlParser(this.props.description)}
         </div>
-        <div className={styles.columns}>
+        {/* <div className={styles.columns}>
           {this.state.columns.map(({key, ...columnProps}) => (
             <Column key={key} {...columnProps} />
           ))}
-        </div>
-        <div className={styles.creator}>
+        </div> */}
+        {/* <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} cancelConfirmation={true}/>
-        </div>
+        </div> */}
       </section>
     );
   }
