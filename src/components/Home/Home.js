5eb0d9cf0304657bdae8e361ac68a2f56979e3b2
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-09-22 13:16:46 +0200

    add ListLink

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import List from '../List/ListContainer';
+import ListLink from '../ListLink/ListLink';
 import styles from './Home.scss';
-// import Creator from '../Creator/Creator';
 import PropTypes from 'prop-types';
-import Search from '../Search/SearchContainer';
 
 class Home extends React.Component {
 
@@ -20,25 +18,11 @@ class Home extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
         <div>
-          {
-            // map -> weź dane z tablicy, na której to wywołujemy, wykonaj transformację na każdym elemencie
-            //  i elementy powstałę po tej transformacji "sklej" jako nową tablicę
-            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-            // https://epicreact.dev/why-react-needs-a-key-prop/ 
-
-            /*
-              {lists.map(kotek => (
-              new List(kotek.id, kotek.title, ...)
-            ))}
-            */
-          }
           {lists.map(listData => (
-            <List key={listData.id} {...listData} />
+            <ListLink key={listData.id} {...listData} />
           ))}
         </div>
-        {/* <Creator text={settings.listCreatorText} action={title => this.addList(title)}/> */}
       </main>
     );
   }
