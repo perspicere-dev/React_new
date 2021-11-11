99afc813123a01747609b9ce33371bcab1ff5304
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-11-11 14:26:48 +0100

    add routing

--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -2,11 +2,11 @@ import {connect} from 'react-redux';
 import List from './List';
 import {getColumnsForList, createActionAddColumn} from '../../Redux/columnsRedux';
 
-const mapStateToProps = (state, props) => {
-  const id = props.match.params.id;
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
   const filteredLists = state.lists.filter(list => list.id == id);
   const listParams = filteredLists[0] || {};
-  console.log('xxx', props);
+  console.log('ownProps', ownProps);
 
   return {
     ...listParams,
@@ -22,3 +22,38 @@ const mapDispatchToProps = (dispatch, props) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(List);
+
+
+
+// const tmpFunc = () => {
+//   const lists = [
+//     {
+//       id: 10,
+//       name: 'test',
+//     },{
+//       id: 12,
+//       name: 'The Answer',
+//     },
+//   ];
+//   const id = 42;
+
+//   const filteredLists = [];
+
+//   // i == 1
+//   for (let i = 0; i < lists.length; i++) {
+//     // list == { if:42, name: 'The Answer'}
+//     const list = lists[i];
+
+//     // list.id == 42
+//     // id == 42
+//     // (42 == 42) == true
+//     if (list.id == id) {
+//       // to się wykona dla i == 1
+//       filteredLists.push(list);
+//     }
+
+//     // kiedy i == 0 to filteredLists == []
+//   }
+
+//   return filteredLists;
+// };
